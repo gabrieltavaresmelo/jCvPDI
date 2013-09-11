1b98b5df8a11c637f31ce69b2e8472a8936c89f7
Gabriel Tavares <gabrieltavaresmelo@gmail.com>
2013-09-11 10:42:02 -0300

    teste2

--- src/main/java/br/com/translate/Main.java
+++ src/main/java/br/com/translate/Main.java
@@ -7,6 +7,7 @@ public class Main {
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {	
 			public void run() {				
+				System.out.println("teste");
 //				MainWindow.getInstance().setVisible(true);
 			}
 		});
